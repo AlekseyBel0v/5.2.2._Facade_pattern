d011ce1c3bed8b9d106f1487ed79cac34c0d71ef
Alexey Belov <bel0valeksey@yandex.ru>
2023-03-02 03:02:49 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("11", "111"));     // 1+2+1+2+4=1=0b1010
+        System.out.println(binOps.mult("11", "11"));
+    }
+}
